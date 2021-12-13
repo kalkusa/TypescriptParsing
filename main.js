400ddfc7aed34848ce462159cbe46ff81d38d532
Arkadiusz Kalkus <kalkus@10g.pl>
2021-12-13 22:23:41 +0100

    Using native parser. Generating code from AST.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const typescriptParser = require("typescript-parser");
-const parser = new typescriptParser.TypescriptParser();
-parser.parseFile("./sample.ts", "").then((parsed) => {
-  console.log(parsed);
-  console.log(JSON.stringify(parsed));
-});
+const ts = require("typescript");
+const fs = require("fs");
+
+const code = fs.readFileSync("./sample.ts").toString();
+const node = ts.createSourceFile("sample2.ts", code, ts.ScriptTarget.LineFeed);
+const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+console.log(node.statements);
+
+for (let index = 0; index < node.statements.length; index++) {
+  const element = node.statements[index];
+  if (element.kind === 257) {
+    console.log("Found interface declaration...");
+    const result = printer.printNode(ts.EmitHint.Unspecified, element, node);
+    console.log(result);
+    console.log("");
+  }
+}
